494573b1b900029a1243605fee1a1ac050a3cd31
Kavyadeep Dev <devkavyadeep@gmail.com>
2020-09-09 16:41:39 +0530

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,9 +15,8 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
-      console.log(this.props.store);
+    .then(response=> response.json())
+    .then(users => {this.setState({ robots: users})});
   }
 
   onSearchChange = (event) => {
